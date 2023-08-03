9b656cae4a64fe9bae7c23f4ae75e669696ceca9
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-08-03 13:34:49 +0100

    Setting up the cached database

--- SpotifyWrapped/SpotifyWrapped/settings.py
+++ SpotifyWrapped/SpotifyWrapped/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
